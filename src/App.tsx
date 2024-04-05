665029db73b2839103df5d0512fc38fc9aac1a63
whalsrl2008 <whalsrl2008@gmail.com>
2024-04-05 21:25:30 +0900

    feat: tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import Experience from "./Components/Experience";
-import "./App.css";
-import { Canvas } from "@react-three/fiber";
-import { useState } from "react";
-import { Texture, TextureLoader } from "three";
+import CropDemo from "./Components/Crop";
 
 // 종이 애니메이션 베이킹 / Pinch 인터랙션 테스트
 // 텍스쳐 변경 기능 테스트
@@ -15,37 +11,45 @@ function App() {
   // Button 두 개 주기
   // Use Default / Use Photo
 
-  const [texture, setTexture] = useState<Texture | undefined>(undefined);
+  // const imageInputRef = useRef<HTMLInputElement>(null);
 
-  const handleTextureInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      const file = e.target.files[0];
-      const imageUrl = URL.createObjectURL(file);
-      const loader = new TextureLoader();
+  // const [texture, setTexture] = useState<Texture | undefined>(undefined);
 
-      loader.load(
-        imageUrl,
-        (loadedTexture) => {
-          setTexture(loadedTexture);
-        },
-        undefined,
-        (error) => {
-          console.error("Error loading texture : ", error);
-        }
-      );
-    }
-  };
+  // const handleTextureInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   if (e.target.files) {
+  //     const file = e.target.files[0];
+  //     const imageUrl = URL.createObjectURL(file);
+  //     const loader = new TextureLoader();
+
+  //     loader.load(
+  //       imageUrl,
+  //       (loadedTexture) => {
+  //         setTexture(loadedTexture);
+  //       },
+  //       undefined,
+  //       (error) => {
+  //         console.error("Error loading texture : ", error);
+  //       }
+  //     );
+  //   }
+  // };
 
   return (
     <>
-      <input
+      <div className="text-3xl">hi</div>
+      {/* <input
         type="file"
         onChange={handleTextureInput}
+        ref={imageInputRef}
         className="absolute ml-auto z-100"
       />
-      <Canvas className="fixed top-0 left-0 w-full h-full overflow-hidden">
+      <button onClick={() => console.log(imageInputRef.current?.value)}>
+        Check data
+      </button> */}
+      <CropDemo />
+      {/* <Canvas className="fixed top-0 left-0 w-full h-full overflow-hidden">
         <Experience texture={texture} />
-      </Canvas>
+      </Canvas> */}
     </>
   );
 }
