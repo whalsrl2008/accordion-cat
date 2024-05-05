5e296384cea638bed735d8a316e086e8707c37f2
whalsrl2008 <whalsrl2008@gmail.com>
2024-05-05 21:08:36 +0900

    feat: animation & models added in scene

--- src/Components/Experience.tsx
+++ src/Components/Experience.tsx
@@ -1,19 +1,23 @@
 import { useRef } from "react";
 import { DoubleSide, Mesh, Texture } from "three";
+import Model from "./Accordion";
 
 type Props = {
   texture?: Texture;
 };
 
 export default function Experience({ texture }: Props) {
-  const cubeRef = useRef<Mesh>(null);
+  // const cubeRef = useRef<Mesh>(null);
 
   return (
     <>
-      <mesh ref={cubeRef} scale={[1, 1, 1]}>
+      {/* <mesh ref={cubeRef} scale={[1, 1, 1]}>
         <planeGeometry args={[2, 2]} />
         {texture && <meshBasicMaterial map={texture} side={DoubleSide} />}
-      </mesh>
+      </mesh> */}
+      <Model />
+      <ambientLight intensity={2.0} />
+      <directionalLight position={[0, 4, 3]} intensity={2.0} castShadow />
     </>
   );
 }
